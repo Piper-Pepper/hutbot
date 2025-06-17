f4d298fb7b168400534f9166a4b08d81ef4ee063
Piper Pepper <piper.pepper.pmv@googlemail.com>
2025-06-17 21:36:03 +0200

    Add files via upload

--- hutbot.py
+++ hutbot.py
@@ -53,6 +53,7 @@ async def main():
         await bot.load_extension("pepper")
         await bot.load_extension("hutmember")
         await bot.load_extension("dm_logger")
+        await bot.load_extension("riddle")
 
         await bot.start(TOKEN)
 
