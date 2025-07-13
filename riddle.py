96fae5e3a65d7490fd20dc804b009aeae475184c
Piper Pepper <piper.pepper.pmv@googlemail.com>
2025-07-13 04:25:39 +0200

    Add files via upload

--- riddle.py
+++ riddle.py
@@ -114,7 +114,7 @@ class ChampionsView(View):
 
                 embed.add_field(
                     name=f"🎖️**{i}.** {display_name}\n*({username})*",
-                    value=f"*🧩 Solved: {solved}*\n📊 **{percent:.1f}%** of all riddles",
+                    value=f"*🧩 Solved: {solved}* / 📊**({percent:.1f}%)**",
                     inline=False
                 )
 
